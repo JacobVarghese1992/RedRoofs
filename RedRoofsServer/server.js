84e10348a4c2aee24b37e402001563eea8dc6e2e
Jacob Varghese <jacobv1992@gmail.com>
2017-04-11 23:10:00 -0400

    Added mail to

--- RedRoofsServer/server.js
+++ RedRoofsServer/server.js
@@ -39,10 +39,13 @@ app.get('/listings/:state/:city', function(req, res) {
   	// res.json(users);
   	console.log(req.params.state)
   	console.log(req.params.city)
-
+    var sendmail1 = "<br><a href='";
+    var sendmail2 = "'>Send Mail</a>";
 	// var query = 'INSERT INTO Listings(listing_id,address,beds,baths,price,currency,safety_rating,link,longitude,latitude,Agent_id) VALUE(?,?,?,?,?,?,?,?,?,?,?)';
     var query = "SELECT L.listing_id,L.address,L.image,L.beds,L.baths,CONCAT(C.symbol,L.price) AS " +
-"price,L.safety_rating,L.link,R.description AS Agent, AM.Amenity, CONCAT('fav-',L.listing_id) AS fav FROM Listings AS L " +
+"price,L.safety_rating,L.link, " + 
+"CONCAT(R.description,sendmail1,R.email_id,sendmail2) AS Agent, " + 
+"AM.Amenity,CONCAT('fav-',L.listing_id) AS fav FROM Listings AS L " +
 "INNER JOIN Currencies AS C " +
 "ON L.currency = C.currency " +
 "INNER JOIN RealEstateAgents AS R " +
